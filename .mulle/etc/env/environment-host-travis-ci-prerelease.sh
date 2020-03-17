7f8b44fb0aec0a94d3493b3557c413c23c70fe36
Nat! <nat@mulle-kybernetik.com>
2020-03-17 23:47:21 +0100

    add missing environment variables

--- .mulle/etc/env/environment-host-travis-ci-prerelease.sh
+++ .mulle/etc/env/environment-host-travis-ci-prerelease.sh
@@ -16,3 +16,21 @@ export MULLE_THREAD_URL="https://github.com/mulle-concurrent/mulle-thread.git"
 export MULLE_THREAD_BRANCH="prerelease"
 
 
+#
+# mulle-c11 nodetype: ${MULLE_C11_NODETYPE:-tar}
+#
+export MULLE_C11_NODETYPE="git"
+
+
+#
+# mulle-c11 url: ${MULLE_C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
+
+
+#
+# mulle-c11 branch: ${MULLE_C11_BRANCH}
+#
+export MULLE_C11_BRANCH="prerelease"
+
+
